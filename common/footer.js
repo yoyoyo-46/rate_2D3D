4b02e22e210c6e6e923670dddae5fd1b00c4f33a
kyz-46 <61731110+yoyoyo-46@users.noreply.github.com>
2023-10-05 08:16:26 +0900

    Add files via upload

--- common/footer.js
+++ common/footer.js
@@ -13,7 +13,6 @@ function footer(){
   html += '<a href="https://yoyoyo-46.github.io/rate_2D3D/footer/profile/profile.html">プロフィール</a>';
   html += '</div>';
   html += '<div class="footer-center">';
-  html += 'らてのサイト';
   html += '</div>';
   html += '</div>';
   html += '</footer>';
